e7098cfa706eefd5d0f7413587177d34f3bde1e9
ShukurDev <shukurdev02@gmail.com>
2021-12-26 13:17:00 +0500

    first change

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from api.models import (
+    Image,
+    Icon,
+    Task,
+    List,
+    Group,
+)
+
+
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_per_page = 25
+
+
+
+admin.site.register(Image, ImageAdmin)
+
+
+class IconAdmin(admin.ModelAdmin):
+    search_fields = ['new_icon', 'created_icon']
+    list_per_page = 50
+
+
+admin.site.register(Icon, IconAdmin)
+
+admin.site.register(Task)
+
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['icon','title']
+    search_fields = ['title']
+    list_per_page = 45
+    ordering = ['title']
+    pass
+
+admin.site.register(List)
+admin.site.register(Group)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Icon(models.Model):
+    icon = models.ImageField(null=True, blank=True, verbose_name='new_icon')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='icon_created')
+
+
+class Image(models.Model):
+    photo = models.ImageField(null=True, verbose_name='new_image')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='photo_created')
+
+
+class Task(models.Model):
+    Date = (
+        ('Today', 'Bugun'),
+        ('Tomorrow', 'Ertaga'),
+        ('Next_week', 'Keyingi hafta'),
+    )
+    title = models.CharField(max_length=300, null=True, blank=True, verbose_name='new_task')
+    text = models.TextField(null=True, blank=True, verbose_name='note')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='task_created')
+    date = models.DateTimeField(choices=Date, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+
+class List(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True, verbose_name='title')
+    icon = models.ForeignKey(Icon, on_delete=models.CASCADE, null=True)
+    photo = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    list = models.ManyToOneRel(Group, field_name='list', to=5)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='list_created')
+
+    def __str__(self):
+        return self.title
